e36fb0a3eda074f044d2296109c5d4669e5c3642
Mpho Matsoetlane <matsoetlanempho45@gmail.com>
2026-09-21 17:39:23 +0200

    Create Q02_BasicArithmetic.java

---
+++ Q02_BasicArithmetic.java
@@ -0,0 +1,23 @@
+public class Q02_BasisArithmetic 
+  {
+    public static void main (String[] args)
+    {
+      int a = 125;
+      int b = 24;
+       int sum = a + b;
+      int difference = a - d;
+      int product = a * b;
+      int quotient = a/ b;
+      int remainder = a % b;
+
+      System.out.println(a + " + " + b + " = " + sum);
+      System.out.println( a + " - "  + b + " = " + difference);
+      System.out.println(a + " x " + b + " = " + product);
+      System.out.println(a + " / " + b + " = " + quotient);
+      System.out.println(a + " mod " + b + " = " + remainder);
+
+    }
+  }
+
+
+
