46686b301310ecfe1739ebc101012050ec839a33
Mpho Matsoetlane <matsoetlanempho45@gmail.com>
2026-09-22 12:58:52 +0200

    Create Q43_IncreasingNumbers.java

---
+++ Q43_IncreasingNumbers.java
@@ -0,0 +1,13 @@
+public class Q43_IncreasingNumbers {
+
+    public static void main(String[] args) {
+
+        int first = 2;
+        int second = 9;
+        int third = 14;
+
+        boolean result = (second > first && third > second);
+
+        System.out.println(result);
+    }
+}
