d0d2a6acc5e01318d3eaa6be070ecd82f3ad208f
Mpho Matsoetlane <matsoetlanempho45@gmail.com>
2026-09-22 12:26:27 +0200

    Add Q31_ASCIIValue class to display ASCII value

---
+++ Q31_ASCIIValue.java
@@ -0,0 +1,11 @@
+public class Q31_ASCIIValue {
+
+    public static void main(String[] args) {
+
+        char character = 'Z';
+
+        int asciiValue = (int) character;
+
+        System.out.println("The ASCII value of " + character + " is: " + asciiValue);
+    }
+}
