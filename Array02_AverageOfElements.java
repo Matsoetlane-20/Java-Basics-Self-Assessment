7d7b46e8521874aaaa0f56bb72a7a757b982e9c8
Mpho Matsoetlane <matsoetlanempho45@gmail.com>
2026-09-22 14:05:45 +0200

    Implement average calculation for array elements

---
+++ Array02_AverageOfElements.java
@@ -0,0 +1,17 @@
+public class Array02_AverageOfElements {
+
+    public static void main(String[] args) {
+
+        int[] numbers = {10, 20, 30, 40, 50};
+
+        int sum = 0;
+
+        for (int number : numbers) {
+            sum = sum + number;
+        }
+
+        double average = (double) sum / numbers.length;
+
+        System.out.println("Average of array elements: " + average);
+    }
+}
