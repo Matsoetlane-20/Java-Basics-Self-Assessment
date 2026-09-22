133f211e61e9213ef1d46a97254d52ebcd221818
Mpho Matsoetlane <matsoetlanempho45@gmail.com>
2026-09-22 12:48:35 +0200

    Add Q41_StringToInteger class to parse string to int

---
+++ Q41_StringToInteger.java
@@ -0,0 +1,11 @@
+public class Q41_StringToInteger {
+
+    public static void main(String[] args) {
+
+        String text = "25";
+
+        int number = Integer.parseInt(text);
+
+        System.out.println("The integer value is: " + number);
+    }
+}
