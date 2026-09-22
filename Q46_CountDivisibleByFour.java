7662a99af6d8ca896b0a02d875d600ea3e402623
Mpho Matsoetlane <matsoetlanempho45@gmail.com>
2026-09-22 13:07:36 +0200

    Create Q46_CountDivisibleByFour.java

---
+++ Q46_CountDivisibleByFour.java
@@ -0,0 +1,16 @@
+public class Q46_CountDivisibleByFour {
+
+    public static void main(String[] args) {
+
+        int count = 0;
+
+        for (int i = 1; i <= 20; i++) {
+
+            if (i % 4 == 0) {
+                count++;
+            }
+        }
+
+        System.out.println("Count: " + count);
+    }
+}
