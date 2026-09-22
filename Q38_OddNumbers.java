4c6da6ef9097981cb3a32fbc97ae5b6b9898f06b
Mpho Matsoetlane <matsoetlanempho45@gmail.com>
2026-09-22 12:44:58 +0200

    Add Q38_OddNumbers.java to print odd numbers

---
+++ Q38_OddNumbers.java
@@ -0,0 +1,10 @@
+public class Q38_OddNumbers {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 99; i += 2) {
+
+            System.out.println(i);
+        }
+    }
+}
