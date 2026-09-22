861d58e34652433dde3adb862a65a36abb983095
Mpho Matsoetlane <matsoetlanempho45@gmail.com>
2026-09-22 12:53:46 +0200

    Create Q42_SumEqualsThird.java

---
+++ Q42_SumEqualsThird.java
@@ -0,0 +1,13 @@
+public class Q42_SumEqualsThird {
+
+    public static void main(String[] args) {
+
+        int first = 5;
+        int second = 10;
+        int third = 15;
+
+        boolean result = (first + second == third);
+
+        System.out.println(result);
+    }
+}
